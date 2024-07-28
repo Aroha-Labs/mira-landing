3aa059d07418bcfb53e2816c6604d0918ae0e8ec
sarim2000 <sarimbleedblue@gmail.com>
2024-07-28 13:57:59 +0530

    fix: cta update from pragun

--- src/components/FirstEcosystemCTA.tsx
+++ src/components/FirstEcosystemCTA.tsx
@@ -34,34 +34,33 @@ const FirstEcosystemCTA = () => {
     }, [inViewPhone]);
 
     return (
-        <div className="w-full md:pl-[73px] mt-[141px] flex max-md:flex-col-reverse max-md:w-full justify-start md:justify-between items-center md:items-start relative">
-            <div
-                className={`relative z-[1] flex flex-col max-md:items-center gap-[24px] md:flex-1 mt-[32px] md:mt-[86px] md:absolute ${
-                    isActive || isActivePhone ? "fade-in-normal" : "opacity-0"
-                }`}
-            >
-                <p
-                    className={`text-[20px] font-semibold leading-[19px] text-[#464C55] w-[167px] text-left max-md:text-center  ${ibmPlexSerif.className}`}
-                >
-                    Klok: Our First ecosystem App
-                </p>
-                <p className="text-[14px] font-normal tracking-[-2%] leading-[17px] text-[#455164] w-[254px] text-left max-md:text-center">
-                    A novel AI ecosystem designed for collaboration, openness,
-                    and unparalleled scalability
-                </p>
-                <Link href={"https://klokapp.ai/login"} target="_blank">
-                    <button className="rounded-full w-fit px-[16px] py-[8px] text-[#FBFDFF] text-[14px] font-medium tracking-[-2%] leading-[17px] bg-[#4F95FF] hover:bg-[#4F88DD] transition-all duration-300">
-                        Try it out now
-                    </button>
-                </Link>
-            </div>
-            <div ref={ref} className="max-md:hidden">
-                <FirstEcosystemSVG isActive={isActive} />
-            </div>
-            <div ref={refPhone} className="md:hidden">
-                <FirstEcosystemPhone isActive={isActivePhone} />
-            </div>
+      <div className="w-full md:pl-[73px] mt-[141px] flex max-md:flex-col-reverse max-md:w-full justify-start md:justify-between items-center md:items-start relative">
+        <div
+          className={`relative z-[1] flex flex-col max-md:items-center gap-[24px] md:flex-1 mt-[32px] md:mt-[86px] md:absolute ${
+            isActive || isActivePhone ? "fade-in-normal" : "opacity-0"
+          }`}
+        >
+          <p
+            className={`text-[20px] font-semibold leading-[19px] text-[#464C55] w-[167px] text-left max-md:text-center  ${ibmPlexSerif.className}`}
+          >
+            Klok: Our First ecosystem App
+          </p>
+          <p className="text-[14px] font-normal tracking-[-2%] leading-[17px] text-[#455164] w-[254px] text-left max-md:text-center">
+            Crypto intelligence on command
+          </p>
+          <Link href={"https://klokapp.ai/login"} target="_blank">
+            <button className="rounded-full w-fit px-[16px] py-[8px] text-[#FBFDFF] text-[14px] font-medium tracking-[-2%] leading-[17px] bg-[#4F95FF] hover:bg-[#4F88DD] transition-all duration-300">
+              Try it out now
+            </button>
+          </Link>
         </div>
+        <div ref={ref} className="max-md:hidden">
+          <FirstEcosystemSVG isActive={isActive} />
+        </div>
+        <div ref={refPhone} className="md:hidden">
+          <FirstEcosystemPhone isActive={isActivePhone} />
+        </div>
+      </div>
     );
 };
 
