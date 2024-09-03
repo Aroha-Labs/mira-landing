c7a79cd47c27cb77c8d8e77c5d1170e391eac409
sarim2000 <sarimbleedblue@gmail.com>
2024-09-03 18:36:21 +0530

    chore(Navbar): update links in Navbar component
    - Updated links to point to correct URLs in the Navbar component
    - Replaced href values with correct URLs for 'research' and 'blog' links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,54 +8,51 @@ const Navbar = ({ onResearchClick }: { onResearchClick: () => void }) => {
     const pathname = usePathname();
 
     return (
-        <div className="fade-in-first relative z-[1] flex w-full justify-start items-center pl-[34px] md:pl-[129px] gap-x-8 text-[#455164] text-[14px] leading-[17px] pb-3 pt-[39px] md:pt-[37px]">
-            <Link href="/">
-                <p
-                    className={cn(
-                        "cursor-pointer transition-all duration-300",
-                        pathname === "/"
-                            ? "text-[#4F95FF]"
-                            : "text-[#455164] hover:text-[#4F95FF]"
-                    )}
-                >
-                    {pathname === "/" ? "*mira" : "+mira"}
-                </p>
-            </Link>
-            {pathname === "/" ? (
-                <p
-                    className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300"
-                    onClick={onResearchClick}
-                >
-                    +research
-                </p>
-            ) : (
-                <Link href="/#research">
-                    <p className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300">
-                        +research
-                    </p>
-                </Link>
+      <div className="fade-in-first relative z-[1] flex w-full justify-start items-center pl-[34px] md:pl-[129px] gap-x-8 text-[#455164] text-[14px] leading-[17px] pb-3 pt-[39px] md:pt-[37px]">
+        <Link href="/">
+          <p
+            className={cn(
+              "cursor-pointer transition-all duration-300",
+              pathname === "/"
+                ? "text-[#4F95FF]"
+                : "text-[#455164] hover:text-[#4F95FF]"
             )}
+          >
+            {pathname === "/" ? "*mira" : "+mira"}
+          </p>
+        </Link>
+        {pathname === "/" ? (
+          <Link href="https://research.mira.network/">
+            <p className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300">
+              +research
+            </p>
+          </Link>
+        ) : (
+          <Link href="https://research.mira.network/">
+            <p className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300">
+              +research
+            </p>
+          </Link>
+        )}
 
-            <Link href="/about">
-                <p
-                    className={cn(
-                        "cursor-pointer transition-all duration-300",
-                        pathname === "/about"
-                            ? "text-[#4F95FF]"
-                            : "text-[#455164] hover:text-[#4F95FF]"
-                    )}
-                >
-                    {pathname === "/about" ? "*about" : "+about"}
-                </p>
-            </Link>
-            <Link
-                href="https://blog.mira.network"
-            >
-                <p className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300">
-                    +blog
-                </p>
-            </Link>
-        </div>
+        <Link href="/about">
+          <p
+            className={cn(
+              "cursor-pointer transition-all duration-300",
+              pathname === "/about"
+                ? "text-[#4F95FF]"
+                : "text-[#455164] hover:text-[#4F95FF]"
+            )}
+          >
+            {pathname === "/about" ? "*about" : "+about"}
+          </p>
+        </Link>
+        <Link href="https://blog.mira.network">
+          <p className="cursor-pointer hover:text-[#4F95FF] transition-all duration-300">
+            +blog
+          </p>
+        </Link>
+      </div>
     );
 };
 
