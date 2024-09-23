58736ae5e895d8ad4251ccb9427e3d46e6ee7288
sarim2000 <sarimbleedblue@gmail.com>
2024-09-23 11:50:40 +0530

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = ({ onResearchClick }: { onResearchClick: () => void }) => {
     const pathname = usePathname();
 
     return (
-      <div className="fade-in-first relative z-[1] flex w-full justify-start items-center pl-[34px] md:pl-[129px] gap-x-8 text-[#455164] text-[14px] leading-[17px] pb-3 pt-[39px] md:pt-[37px]">
+      <div className="fade-in-first relative z-[1] flex w-full justify-start items-center pl-[34px] md:pl-[129px] gap-x-8 text-[#455164] text-[14px] leading-[17px] pb-3 pt-[39px] md:pt-[37px] flex-wrap gap-y-2">
         <Link href="/">
           <p
             className={cn(
